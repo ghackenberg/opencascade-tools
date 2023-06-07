38c9e15baf23cf194bddd6a7931a4b510421b90c
Georg Hackenberg <ghackenberg@gmail.com>
2023-06-07 19:54:17 +0200

    measure and print out init time for webassembly

--- src/lib/init/browser.ts
+++ src/lib/init/browser.ts
@@ -3,9 +3,13 @@ import initOpenCascade from 'opencascade.js/dist/index.js'
 export async function init() {
     console.log("Initializing WebAssembly version of OpenCascade")
 
+    const start = Date.now()
+
     const oc = await initOpenCascade()
 
-    console.log("WebAssembly version of OpenCascade initialized successfully")
+    const end = Date.now()
+
+    console.log(`WebAssembly version of OpenCascade initialized successfully in ${end - start} ms`)
 
     return oc
 }
--- src/lib/init/node.ts
+++ src/lib/init/node.ts
@@ -3,9 +3,13 @@ import initOpenCascade from 'opencascade.js/dist/node.js'
 export async function init() {
     console.log("Initializing WebAssembly version of OpenCASCADE Technology")
 
+    const start = Date.now()
+
     const oc = await initOpenCascade()
 
-    console.log("WebAssembly version of OpenCASCADE Technology initialized successfully")
+    const end = Date.now()
+
+    console.log(`WebAssembly version of OpenCASCADE Technology initialized successfully in ${end - start} ms`)
 
     return oc
 }
